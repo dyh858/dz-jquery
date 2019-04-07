60cc5aad1fcd9c32087573fb08369f847e1a1dfd
dyh858 <dyh858@163.com>
2019-04-07 10:50:36 +0800

    20190406自动生成节点响应事件

--- js/demo.js
+++ js/demo.js
@@ -4,7 +4,26 @@ $(function(){
 		url:'data/emp.html',
 		dataType:'json',
 		success:function(response,status,xhr){
-			alert(response[0]["name"]);
+			$('#test').append('<li id="k">点我</li>')
 		}
 	});
+
+/*
+	$(".move").click(function(){
+		alert("点到我move了！");
+	});
+	$(".move").click();  //会触发
+*/
+/*
+	$(document).delegate("#k","click",function(){
+		alert("点到我了kk");
+		console.log(111);  
+	});
+	$('#k').click(); //不会触发
+*/
+	$('#test').on('click','#k',function(){
+		alert("点到我了k2k");
+		
+	});
+	$('#k').trigger();//不会触发
 });
